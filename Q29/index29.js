32bec228d9ae5a8c66f8b5692c17b686f301c70a
munim <muhammadmunimoff330@gmail.com>
2024-04-30 14:17:15 +0500

    Q29

--- Q29/index29.js
+++ Q29/index29.js
@@ -1,13 +1,22 @@
-// Write a function called truncateString that takes two parameters.
+// NUmber Range Generator
 
-// str: A string that meeds to be truncated.
-// maxLength: An integer representing the maximum length of the string allowed.
-// ? The func should truncate the input str if its length exceeds the specified maxLength. if truncation occurs, the func should add '...' to the end of the truncated str.
+// Write function called numberRange that generates an array containing consecutive numbers from a to b (inclusive).
+
+// a: An integer representing the starting number of the range.
+// b: An integer representing the ending number of the range.
+// arr: An optional parameter representing the array to store the number in the range. It defaults to an empty array.
 
 
-const truncateStr = (str, count) => {
-  return count <= 0 ? str : str.slice(0, count).concat("...");
+const numberRange = (a, b) => {
+  let arr = [];
+  while (a <= b) {
+    arr.push(a);
+    a++;
+  }
+  return arr;
 }
 
 
-console.log(truncateStr('A-tisket a-tasket A green and yellow basket', 0));
+console.log(numberRange(0, 5));
+console.log(numberRange(0, 7));
+console.log(numberRange(3, 19));
