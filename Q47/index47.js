7b0f87b9f01407eed9cabb4ece2228c2c75f5bc5
munim <muhammadmunimoff330@gmail.com>
2024-05-16 10:20:37 +0500

    Problem 47

---
+++ Q47/index47.js
@@ -0,0 +1,13 @@
+// Extract numbers from a string
+
+const extractNum = (num) => {
+  let regex = /\d+/g;
+  return num.match(regex);
+}
+
+
+console.log(extractNum("abcd324kln43l"));
+console.log(extractNum("kui num nahi yahan"));
+console.log(extractNum("1a2s3d4f5g6h7j8k9l"));
+console.log(extractNum("122,344,444,22,ans"));
+console.log(extractNum(""));
